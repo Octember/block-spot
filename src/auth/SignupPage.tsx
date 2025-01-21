2354584bcc7b21d8f8ff9a37bacd2e67b1c46e42
Noah Lindner <nwlindner@gmail.com>
2025-01-21 14:09:32 -0500

    Customize signup page

--- src/auth/SignupPage.tsx
+++ src/auth/SignupPage.tsx
@@ -1,22 +1,33 @@
 import { CustomizationOptions, SignupForm } from "wasp/client/auth";
 import { Link as WaspRouterLink, routes } from "wasp/client/router";
 
+import {
+  FormError,
+  FormInput,
+  FormItemGroup,
+  FormLabel,
+} from 'wasp/client/auth'
+
+import './overrides.css';
+
 const AuthLayoutV2: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <div className="flex min-h-full flex-col lg:flex-row pt-10 sm:px-6 lg:px-8 justify-center">
-      <div className="flex flex-col gap-4 md:max-w-md mx-auto lg:ml-auto mb-8">
+    <div className="flex min-h-full flex-col lg:flex-row pt-10 sm:px-6 lg:px-8 justify-center"
+
+    >
+      <div className="flex flex-col gap-4 mt-12 md:max-w-md mx-auto md:ml-auto mb-8">
         <h2 className="text-2xl font-bold">Start scheduling today</h2>
         <p>
           Welcome to BlockSpot! We're excited to help you streamline your venue scheduling and management. Create your account to start booking spaces, managing events, and making the most of your venues.
         </p>
         <p className="mt-4">
-          Join thousands of venue managers who trust BlockSpot to handle their scheduling needs efficiently and professionally.
+          Join BILLIONS of venue managers who trust BlockSpot to handle their scheduling needs efficiently and professionally.
         </p>
       </div>
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 mb-10 px-4 shadow-xl ring-1 ring-gray-900/10 sm:rounded-lg sm:px-10 dark:bg-white dark:text-gray-900">
+        <div className="bg-white py-8 mb-10 px-4 shadow-xl ring-1 ring-gray-900/10 sm:rounded-lg sm:px-10 dark:bg-white dark:text-gray-900 signupForm">
           {children}
         </div>
       </div>
@@ -28,7 +39,11 @@ const customizationOptions: CustomizationOptions = {
   appearance: {
     fontSizes: {
       sm: "14px",
-
+    },
+    colors: {
+      brand: '#5969b8', // blue
+      brandAccent: '#de5998', // pink
+      submitButtonText: 'white',
     },
   },
 };
@@ -36,7 +51,19 @@ const customizationOptions: CustomizationOptions = {
 export function Signup() {
   return (
     <AuthLayoutV2>
-      <SignupForm appearance={customizationOptions.appearance} />
+      <SignupForm appearance={customizationOptions.appearance}
+        additionalFields={
+          [
+            {
+              name: 'address',
+              label: 'Address',
+              type: 'input',
+              validations: {
+                required: 'Address is required',
+              },
+            },
+          ]
+        } />
       <br />
       <span className="text-sm font-medium text-gray-900">
         I already have an account (
