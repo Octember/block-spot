d1e37868db085a0f07bc43f39d210fe7e4980b65
Noah Lindner <nwlindner@gmail.com>
2025-02-16 21:18:25 -0500

    Sentry perf

--- src/client/setup.ts
+++ src/client/setup.ts
@@ -1,8 +1,21 @@
 import * as Sentry from "@sentry/react";
+import { useEffect } from "react";
+import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from "react-router-dom";
+
 
 Sentry.init({
   dsn: "https://7aa6c270ff9ea23691682ec723f49cc8@o4508678930169856.ingest.us.sentry.io/4508679034306560",
-  integrations: [],
+  integrations: [
+    Sentry.browserTracingIntegration(),
+
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
 });
 
 export const setupClient = () => {
