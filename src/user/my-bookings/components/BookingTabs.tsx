a3b9273ea9f09c5902ded9a0efe7b9c121d93b9e
Noah Lindner <nwlindner@gmail.com>
2025-03-04 16:55:07 -0500

    color updates

--- src/user/my-bookings/components/BookingTabs.tsx
+++ src/user/my-bookings/components/BookingTabs.tsx
@@ -13,8 +13,8 @@ export const BookingTabs = ({ activeTab, setActiveTab }: BookingTabsProps) => {
           <button
             onClick={() => setActiveTab("upcoming")}
             className={`${activeTab === "upcoming"
-                ? "border-yellow-500 text-yellow-600"
-                : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+              ? "border-teal-500 text-teal-600"
+              : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
               } whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm`}
           >
             Upcoming
@@ -22,8 +22,8 @@ export const BookingTabs = ({ activeTab, setActiveTab }: BookingTabsProps) => {
           <button
             onClick={() => setActiveTab("past")}
             className={`${activeTab === "past"
-                ? "border-yellow-500 text-yellow-600"
-                : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+              ? "border-teal-500 text-teal-600"
+              : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
               } whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm`}
           >
             Past
