5644a4c434848ca39ffcd5377b0dad9bac77efef
Noah Lindner <nwlindner@gmail.com>
2025-03-03 21:19:20 -0500

    Cleanup form

--- src/schedule/calendar/action-section/forms/reservation-form.tsx
+++ src/schedule/calendar/action-section/forms/reservation-form.tsx
@@ -1,18 +1,16 @@
 import { FC } from "react";
-import { ReservationFormBase } from "./reservation-basics-form";
-import { useAuthUser } from "../../../../auth/providers/AuthUserProvider";
-import { Reservation } from "wasp/entities";
 import { useFormContext } from "react-hook-form";
+import { Reservation } from "wasp/entities";
+import { useAuthUser } from "../../../../auth/providers/AuthUserProvider";
 import { CreateReservationFormInputs } from "../modals/types";
-import { PriceBreakdownDisplay } from "./payments-form";
-import { useParams } from "react-router-dom";
+import { ReservationFormBase } from "./reservation-basics-form";
+import { UpdateReservationUserSection } from "./update-user-section";
 
 export const ReservationForm: FC<{
   reservation: Reservation;
   onSubmit: (data: CreateReservationFormInputs) => void;
 }> = ({ reservation, onSubmit }) => {
   const { isOwner, isAdmin } = useAuthUser();
-  const { venueId } = useParams();
 
   const enableUserSection = isOwner && isAdmin;
   const { handleSubmit } = useFormContext<CreateReservationFormInputs>();
@@ -24,13 +22,7 @@ export const ReservationForm: FC<{
     >
       <ReservationFormBase reservation={reservation} />
 
-      <PriceBreakdownDisplay
-        spaceId={reservation.spaceId}
-        venueId={venueId || ""}
-        startTime={reservation.startTime}
-        endTime={reservation.endTime}
-      />
-      {/* {enableUserSection && <UpdateReservationUserSection />} */}
+      {enableUserSection && <UpdateReservationUserSection />}
     </form>
   );
 };
